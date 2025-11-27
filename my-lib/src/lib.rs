46a1d7576ae83135d6c1ae2d223fd06b69b4cb96
itytophile <imran.tierce@protonmail.com>
2025-11-27 22:25:43 +0100

    res_b_r

--- my-lib/src/lib.rs
+++ my-lib/src/lib.rs
@@ -23,3 +23,20 @@ impl<'a, T: Copy> WriteOnce<'a, T> {
         *self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_bad_add() {
+        let start = 0x80;
+        for b in 0..8 {
+            for (index, r) in ['B', 'C', 'D', 'E', 'H', 'L', 'A'].into_iter().enumerate() {
+                let index = if r == 'A' { index + 1 } else { index };
+                println!(
+                    "0x{:02x} => res_b_r({b}, {r}),",
+                    start + index + (b / 2) * 0x10 + if b % 2 == 1 { 0x8 } else { 0 }
+                );
+            }
+        }
+    }
+}
