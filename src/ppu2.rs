21367ddde8843099e5e6e30056022f201bb8ff41
itytophile <imran.tierce@protonmail.com>
2025-11-30 19:01:07 +0100

    ObjectAttribute

--- src/ppu2.rs
+++ src/ppu2.rs
@@ -1,3 +1,5 @@
+use bitflags::Flags;
+
 use crate::{
     StateMachine,
     state::{State, WriteOnlyState},
@@ -5,10 +7,12 @@ use crate::{
 
 pub struct Ppu2;
 
+// Tile data
+
 const TILE_LENGTH: u8 = 16;
 
 type TileVram = [u8; 0x1800];
-type Tile = [u8; TILE_LENGTH as usize];
+type Tile = [u8; 16];
 type Line = [u8; 2];
 
 pub enum ColorIndex {
@@ -46,6 +50,42 @@ fn get_bg_win_tile(vram: &TileVram, index: u8, is_signed_addressing: bool) -> &T
     vram[base..base + usize::from(TILE_LENGTH)]
         .try_into()
         .unwrap()
+}
+
+// Tile maps
+
+type TileMap = [u8; 0x400]; // 32 * 32 Tile indexes
+
+// OAM
+
+type OAM = [u8; 0xa0];
+
+bitflags::bitflags! {
+    #[derive(Debug, Clone, Default, Copy, PartialEq, Eq)]
+    pub struct ObjectFlags: u8 {
+        const PRIORITY = 1 << 7;
+        const Y_FLIP = 1 << 6;
+        const X_FLIP = 1 << 5;
+        const DMG_PALETTE = 1 << 4;
+    }
+}
+
+struct ObjectAttribute {
+    y: u8,
+    x: u8,
+    tile_index: u8,
+    flags: ObjectFlags,
+}
+
+impl From<[u8; 4]> for ObjectAttribute {
+    fn from([y, x, tile_index, flags]: [u8; 4]) -> Self {
+        Self {
+            y,
+            x,
+            tile_index,
+            flags: ObjectFlags::from_bits_retain(flags),
+        }
+    }
 }
 
 impl StateMachine for Ppu2 {
