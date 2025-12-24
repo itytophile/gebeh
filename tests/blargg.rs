73982de052a7d46970bd94bd844ca2a720aa92b3
itytophile <imran.tierce@protonmail.com>
2025-12-25 00:35:15 +0100

    fix cpu_instrs test

--- tests/blargg.rs
+++ tests/blargg.rs
@@ -1,13 +1,6 @@
 use crate::common::{TestSerial, machine_to_serial_iter};
 use arrayvec::ArrayVec;
-use gb_core::{
-    StateMachine,
-    cpu::Cpu,
-    ppu::{Ppu, Speeder},
-    state::State,
-    timer::Timer,
-};
-use std::num::NonZeroU8;
+use gb_core::{Emulator, StateMachine, state::State};
 
 mod common;
 
@@ -19,11 +12,7 @@ fn cpu_instrs() {
     let rom =
         std::fs::read("/home/ityt/Documents/git/gb-test-roms/cpu_instrs/cpu_instrs.gb").unwrap();
     let mut state = State::new(rom.leak());
-    // the machine should not be affected by the composition order
-    let mut machine = Cpu::default()
-        .compose(Timer)
-        .compose(Speeder(Ppu::default(), NonZeroU8::new(4).unwrap()))
-        .compose(TestSerial(None));
+    let mut machine = Emulator::default().compose(TestSerial(None));
 
     let buffer: ArrayVec<u8, LEN> = machine_to_serial_iter(&mut machine, &mut state)
         .take(LEN)
