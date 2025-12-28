a34243749857bc7f2d816c11454fa4d4a5ce7506
itytophile <imran.tierce@protonmail.com>
2025-12-28 17:33:24 +0100

    new roms commented

--- src/main.rs
+++ src/main.rs
@@ -44,10 +44,12 @@ fn main() {
     // let rom =
     //     std::fs::read("/home/ityt/Documents/git/gb-test-roms/interrupt_time/interrupt_time.gb")
     //         .unwrap();
-    let rom = std::fs::read("/home/ityt/Téléchargements/dmg-acid2.gb").unwrap();
+    // let rom = std::fs::read("/home/ityt/Téléchargements/dmg-acid2.gb").unwrap();
     // let rom = std::fs::read("/home/ityt/Téléchargements/pocket/pocket.gb").unwrap();
+    // let rom = std::fs::read("/home/ityt/Téléchargements/gejmboj/gejmboj.gb").unwrap();
+    let rom = std::fs::read("/home/ityt/Téléchargements/oh_2/oh.gb").unwrap();
     // let rom = std::fs::read(
-    //     "/home/ityt/Téléchargements/mts-20240926-1737-443f6e1/acceptance/ppu/hblank_ly_scx_timing-GS.gb",
+    //     "/home/ityt/Téléchargements/mts-20240926-1737-443f6e1/acceptance/ppu/intr_2_mode0_timing_sprites.gb",
     // )
     // .unwrap();
     // let rom =
