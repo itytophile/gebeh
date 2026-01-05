945bc212d227d634bdb6c4500e7b006bf09349da
itytophile <imran.tierce@protonmail.com>
2026-01-05 22:34:43 +0100

    extract envelope

---
+++ gebeh-core/src/apu/envelope.rs
@@ -0,0 +1,84 @@
+#[derive(Clone, Default)]
+struct EnvelopeTimer {
+    falling_edge: bool,
+    value: u8, // 4 bits
+    is_increasing: bool,
+    sweep_pace: u8, // 3 bits
+    pace_count: u8,
+}
+
+impl EnvelopeTimer {
+    fn trigger(&mut self, volume_and_envelope: u8) {
+        self.is_increasing = volume_and_envelope & 0x08 != 0;
+        self.value = (volume_and_envelope >> 4) & 0x0f;
+        self.sweep_pace = volume_and_envelope & 0x07;
+        self.pace_count = 0;
+    }
+    fn tick(&mut self, div: u8) {
+        // https://gbdev.io/pandocs/Audio_Registers.html#ff12--nr12-channel-1-volume--envelope
+        // A setting of 0 disables the envelope.
+        if self.sweep_pace == 0 {
+            return;
+        }
+
+        // 64 Hz
+        let has_ticked = div & (1 << 7) != 0;
+        if self.falling_edge == has_ticked {
+            return;
+        }
+
+        self.falling_edge = has_ticked;
+
+        if self.falling_edge {
+            return;
+        }
+
+        self.pace_count += 1;
+
+        if self.pace_count != self.sweep_pace {
+            return;
+        }
+
+        self.pace_count = 0;
+
+        match (self.is_increasing, self.value) {
+            (true, 0x0f) | (false, 0) => {}
+            (true, _) => self.value += 1,
+            (false, _) => self.value -= 1,
+        }
+    }
+}
+
+
+#[derive(Clone, Default)]
+pub struct VolumeAndEnvelope {
+    timer: EnvelopeTimer,
+    register: u8,
+}
+
+impl VolumeAndEnvelope {
+    pub fn is_dac_on(&self) -> bool {
+        // https://gbdev.io/pandocs/Audio_details.html#dacs
+        self.register & 0xf8 != 0
+    }
+
+    pub fn get_volume(&self) -> f32 {
+        self.timer.value as f32 / 15.
+    }
+
+    pub fn get_register(&self) -> u8 {
+        self.register
+    }
+
+    pub fn write_register(&mut self, value: u8) {
+        self.register = value;
+    }
+
+    pub fn trigger(&mut self) {
+        self.timer.trigger(self.register);
+    }
+
+    pub fn tick(&mut self, div: u8) {
+        self.timer.tick(div);
+    }
+}
--- gebeh-core/src/apu/mod.rs
+++ gebeh-core/src/apu/mod.rs
@@ -1,6 +1,7 @@
-use crate::apu::length::Length;
+use crate::apu::{envelope::VolumeAndEnvelope, length::Length};
 
 mod length;
+mod envelope;
 
 type Wave = [f32; 8];
 
@@ -191,56 +192,7 @@ impl LengthTimer {
     }
 }
 
-#[derive(Clone, Default)]
-struct EnvelopeTimer {
-    falling_edge: bool,
-    value: u8, // 4 bits
-    is_increasing: bool,
-    sweep_pace: u8, // 3 bits
-    pace_count: u8,
-}
 
-impl EnvelopeTimer {
-    fn trigger(&mut self, volume_and_envelope: u8) {
-        self.is_increasing = volume_and_envelope & 0x08 != 0;
-        self.value = (volume_and_envelope >> 4) & 0x0f;
-        self.sweep_pace = volume_and_envelope & 0x07;
-        self.pace_count = 0;
-    }
-    fn tick(&mut self, div: u8) {
-        // https://gbdev.io/pandocs/Audio_Registers.html#ff12--nr12-channel-1-volume--envelope
-        // A setting of 0 disables the envelope.
-        if self.sweep_pace == 0 {
-            return;
-        }
-
-        // 64 Hz
-        let has_ticked = div & (1 << 7) != 0;
-        if self.falling_edge == has_ticked {
-            return;
-        }
-
-        self.falling_edge = has_ticked;
-
-        if self.falling_edge {
-            return;
-        }
-
-        self.pace_count += 1;
-
-        if self.pace_count != self.sweep_pace {
-            return;
-        }
-
-        self.pace_count = 0;
-
-        match (self.is_increasing, self.value) {
-            (true, 0x0f) | (false, 0) => {}
-            (true, _) => self.value += 1,
-            (false, _) => self.value -= 1,
-        }
-    }
-}
 
 #[derive(Clone, Default)]
 pub struct PulseChannel<S: Sweep> {
@@ -514,8 +466,7 @@ impl Apu {
 struct NoiseChannel {
     nr41: u8,
     length_timer: LengthTimer,
-    nr42: u8,
-    envelope_timer: EnvelopeTimer,
+    volume_and_envelope: VolumeAndEnvelope,
     nr43: u8,
     nr44: u8,
     is_enabled: bool,
@@ -531,10 +482,10 @@ impl NoiseChannel {
         0xff
     }
     fn write_nr42(&mut self, value: u8) {
-        self.nr42 = value;
+        self.volume_and_envelope.write_register(value);
     }
     fn read_nr42(&self) -> u8 {
-        self.nr42
+        self.volume_and_envelope.get_register()
     }
     fn write_nr43(&mut self, value: u8) {
         self.nr43 = value;
@@ -555,18 +506,14 @@ impl NoiseChannel {
     fn trigger(&mut self) {
         self.is_enabled = true;
         self.length_timer.trigger(self.nr41 & 0x3f);
-        self.envelope_timer.trigger(self.nr42);
+        self.volume_and_envelope.trigger();
         self.lfsr.trigger();
     }
 
     fn is_on(&self) -> bool {
-        self.is_dac_on() && self.is_enabled
+        self.volume_and_envelope.is_dac_on() && self.is_enabled
     }
-
-    fn is_dac_on(&self) -> bool {
-        // https://gbdev.io/pandocs/Audio_details.html#dacs
-        self.nr42 & 0xf8 != 0
-    }
+    
     fn tick(&mut self, div: u8) {
         self.inner_clock = self.inner_clock.wrapping_add(1);
         self.lfsr.tick(self.is_short_mode());
@@ -610,38 +557,5 @@ impl LinearFeedbackShiftRegister {
 
     fn trigger(&mut self) {
         self.0 = 0;
-    }
-}
-
-#[derive(Clone, Default)]
-struct VolumeAndEnvelope {
-    timer: EnvelopeTimer,
-    register: u8,
-}
-
-impl VolumeAndEnvelope {
-    fn is_dac_on(&self) -> bool {
-        // https://gbdev.io/pandocs/Audio_details.html#dacs
-        self.register & 0xf8 != 0
-    }
-
-    fn get_volume(&self) -> f32 {
-        self.timer.value as f32 / 15.
-    }
-
-    fn get_register(&self) -> u8 {
-        self.register
-    }
-
-    fn write_register(&mut self, value: u8) {
-        self.register = value;
-    }
-
-    fn trigger(&mut self) {
-        self.timer.trigger(self.register);
-    }
-
-    fn tick(&mut self, div: u8) {
-        self.timer.tick(div);
     }
 }
