219aac022233b1a13ad7c5da6f7fede02f56b3a5
itytophile <imran.tierce@protonmail.com>
2026-01-15 23:40:51 +0100

    simpler events

--- gebeh-web/ts/buttons.ts
+++ gebeh-web/ts/buttons.ts
@@ -21,14 +21,15 @@ export function addButtons(port: MessagePort) {
     [buttonStart, "start"],
     [buttonSelect, "select"],
   ] as const) {
-    element.addEventListener("touchstart", () => {
+    element.addEventListener("pointerdown", (event) => {
+      event.preventDefault();
       port.postMessage({
         type: "input",
         event: "down",
         button,
       } satisfies FromMainMessage);
     });
-    element.addEventListener("touchend", () => {
+    element.addEventListener("pointerup", () => {
       port.postMessage({
         type: "input",
         event: "up",
@@ -37,59 +38,65 @@ export function addButtons(port: MessagePort) {
     });
   }
 
-  dpad.addEventListener("touchstart", (event) => {
-    touchDpad(dpad.getBoundingClientRect(), event, port, true);
-  });
-
-  dpad.addEventListener("touchend", (event) => {
-    touchDpad(dpad.getBoundingClientRect(), event, port, false);
-  });
-}
-
-let dpadState = {
-  left: false,
-  right: false,
-  up: false,
-  down: false,
-};
-
-function touchDpad(
-  rect: DOMRect,
-  event: TouchEvent,
-  port: MessagePort,
-  isStart: boolean,
-) {
-  const newDpadState = {
+  let dpadState = {
     left: false,
     right: false,
     up: false,
     down: false,
   };
 
-  for (const touch of event.touches) {
-    const x = (touch.clientX - rect.left) / rect.width;
-    const y = (touch.clientY - rect.top) / rect.height;
+  dpad.addEventListener("pointerdown", (event) => {
+    event.preventDefault();
 
-    newDpadState.left ||= x < 1 / 3;
-    newDpadState.right ||= x > 2 / 3;
-    newDpadState.up ||= y < 1 / 3;
-    newDpadState.down ||= y > 2 / 3;
-  }
+    const rect = dpad.getBoundingClientRect();
+    const x = (event.clientX - rect.left) / rect.width;
+    const y = (event.clientY - rect.top) / rect.height;
 
-  for (const [before, now, button] of [
-    [dpadState.left, newDpadState.left, "left"],
-    [dpadState.right, newDpadState.right, "right"],
-    [dpadState.up, newDpadState.up, "up"],
-    [dpadState.down, newDpadState.down, "down"],
-  ] as const) {
-    if (before !== now && isStart === now) {
-      port.postMessage({
-        type: "input",
-        event: isStart ? "down" : "up",
-        button,
-      } satisfies FromMainMessage);
+    const newDpadState = {
+      left: x < 1 / 3,
+      right: x > 2 / 3,
+      up: y < 1 / 3,
+      down: y > 2 / 3,
+    };
+
+    for (const [before, now, button] of [
+      [dpadState.left, newDpadState.left, "left"],
+      [dpadState.right, newDpadState.right, "right"],
+      [dpadState.up, newDpadState.up, "up"],
+      [dpadState.down, newDpadState.down, "down"],
+    ] as const) {
+      if (before !== now && now) {
+        port.postMessage({
+          type: "input",
+          event: "down",
+          button,
+        } satisfies FromMainMessage);
+      }
     }
-  }
 
-  dpadState = newDpadState;
+    dpadState = newDpadState;
+  });
+
+  dpad.addEventListener("pointerup", () => {
+    for (const [before, button] of [
+      [dpadState.left, "left"],
+      [dpadState.right, "right"],
+      [dpadState.up, "up"],
+      [dpadState.down, "down"],
+    ] as const) {
+      if (before) {
+        port.postMessage({
+          type: "input",
+          event: "up",
+          button,
+        } satisfies FromMainMessage);
+      }
+    }
+    dpadState = {
+      left: false,
+      right: false,
+      up: false,
+      down: false,
+    };
+  });
 }
