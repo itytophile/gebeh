738dcc4d7b70367462ee97b36ac5ff6b68de8a08
itytophile <imran.tierce@protonmail.com>
2026-01-17 00:33:21 +0100

    can slide on dpad

--- gebeh-web/ts/buttons.ts
+++ gebeh-web/ts/buttons.ts
@@ -53,56 +53,68 @@ export function addButtons(port: MessagePort) {
 
   dpad.addEventListener("pointerdown", (event) => {
     event.preventDefault();
+    // to be able to move the finger while pressing and change buttons
+    dpad.setPointerCapture(event.pointerId);
 
+    handlePress(event);
+  });
+
+  dpad.addEventListener("pointermove", handlePress);
+  dpad.addEventListener("pointerup", cancelPointer);
+  dpad.addEventListener("pointercancel", cancelPointer);
+
+  function cancelPointer(event: PointerEvent) {
+    dpad.releasePointerCapture(event.pointerId);
+    applyDpadState({ down: false, left: false, right: false, up: false });
+  }
+
+  function handlePress(event: PointerEvent) {
     const rect = dpad.getBoundingClientRect();
+
+    const inside =
+      event.clientX >= rect.left &&
+      event.clientX <= rect.right &&
+      event.clientY >= rect.top &&
+      event.clientY <= rect.bottom;
+
+    if (!inside) {
+      cancelPointer(event);
+      return;
+    }
+
     const x = (event.clientX - rect.left) / rect.width;
     const y = (event.clientY - rect.top) / rect.height;
 
-    const newDpadState = {
+    applyDpadState({
       left: x < 1 / 3,
       right: x > 2 / 3,
       up: y < 1 / 3,
       down: y > 2 / 3,
-    };
+    });
+  }
 
+  function applyDpadState(newDpadState: {
+    left: boolean;
+    right: boolean;
+    up: boolean;
+    down: boolean;
+  }) {
     for (const [before, now, button] of [
       [dpadState.left, newDpadState.left, "left"],
       [dpadState.right, newDpadState.right, "right"],
       [dpadState.up, newDpadState.up, "up"],
       [dpadState.down, newDpadState.down, "down"],
     ] as const) {
-      if (before !== now && now) {
-        port.postMessage({
-          type: "input",
-          event: "down",
-          button,
-        } satisfies FromMainMessage);
+      if (before === now) {
+        continue;
       }
+      port.postMessage({
+        type: "input",
+        event: now ? "down" : "up",
+        button,
+      } satisfies FromMainMessage);
     }
 
     dpadState = newDpadState;
-  });
-
-  dpad.addEventListener("pointerup", () => {
-    for (const [before, button] of [
-      [dpadState.left, "left"],
-      [dpadState.right, "right"],
-      [dpadState.up, "up"],
-      [dpadState.down, "down"],
-    ] as const) {
-      if (before) {
-        port.postMessage({
-          type: "input",
-          event: "up",
-          button,
-        } satisfies FromMainMessage);
-      }
-    }
-    dpadState = {
-      left: false,
-      right: false,
-      up: false,
-      down: false,
-    };
-  });
+  }
 }
