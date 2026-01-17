8855057bccfb6dc603c987679dbbe446fb8ec20d
itytophile <imran.tierce@protonmail.com>
2026-01-17 20:03:56 +0100

    force stereo

--- src/emulator_loop.rs
+++ src/emulator_loop.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use cpal::{
-    FromSample, I24, SizedSample,
+    BufferSize, FromSample, I24, SizedSample, StreamConfig,
     traits::{DeviceTrait, StreamTrait},
 };
 use gebeh::InstantRtc;
@@ -24,40 +24,40 @@ pub fn spawn_emulator(
     let config = device.default_output_config().unwrap();
     let stream = match config.sample_format() {
         cpal::SampleFormat::I8 => {
-            create_stream::<i8>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<i8>(device, config.into(), shared_frame, shared_joypad)
         }
         cpal::SampleFormat::I16 => {
-            create_stream::<i16>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<i16>(device, config.into(), shared_frame, shared_joypad)
         }
         cpal::SampleFormat::I24 => {
-            create_stream::<I24>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<I24>(device, config.into(), shared_frame, shared_joypad)
         }
         cpal::SampleFormat::I32 => {
-            create_stream::<i32>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<i32>(device, config.into(), shared_frame, shared_joypad)
         }
         // cpal::SampleFormat::I48 => run::<I48>(&device, &config.into(),shared_frame),
         cpal::SampleFormat::I64 => {
-            create_stream::<i64>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<i64>(device, config.into(), shared_frame, shared_joypad)
         }
         cpal::SampleFormat::U8 => {
-            create_stream::<u8>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<u8>(device, config.into(), shared_frame, shared_joypad)
         }
         cpal::SampleFormat::U16 => {
-            create_stream::<u16>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<u16>(device, config.into(), shared_frame, shared_joypad)
         }
         // cpal::SampleFormat::U24 => run::<U24>(&device, &config.into(),shared_frame),
         cpal::SampleFormat::U32 => {
-            create_stream::<u32>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<u32>(device, config.into(), shared_frame, shared_joypad)
         }
         // cpal::SampleFormat::U48 => run::<U48>(&device, &config.into(),shared_frame),
         cpal::SampleFormat::U64 => {
-            create_stream::<u64>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<u64>(device, config.into(), shared_frame, shared_joypad)
         }
         cpal::SampleFormat::F32 => {
-            create_stream::<f32>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<f32>(device, config.into(), shared_frame, shared_joypad)
         }
         cpal::SampleFormat::F64 => {
-            create_stream::<f64>(device, &config.into(), shared_frame, shared_joypad)
+            create_stream::<f64>(device, config.into(), shared_frame, shared_joypad)
         }
         sample_format => panic!("Unsupported sample format '{sample_format}'"),
     };
@@ -68,21 +68,13 @@ pub fn spawn_emulator(
 
 fn create_stream<T>(
     device: &cpal::Device,
-    config: &cpal::StreamConfig,
+    config: cpal::StreamConfig,
     shared_frame: Arc<RwLock<[Color; WIDTH as usize * HEIGHT as usize]>>,
     shared_joypad: Arc<RwLock<JoypadInput>>,
 ) -> cpal::Stream
 where
     T: SizedSample + FromSample<f32>,
 {
-    let sample_rate = config.sample_rate;
-    let channels = config.channels as usize;
-    println!("Channel count: {channels}");
-    let mut sample_index = 0u32;
-
-    let noise = get_noise(false);
-    let short_noise = get_noise(true);
-
     // let rom = std::fs::read("/home/ityt/Téléchargements/dmg-acid2.gb").unwrap();
     // let rom = std::fs::read("/home/ityt/Téléchargements/pocket/pocket.gb").unwrap();
     // let rom = std::fs::read("/home/ityt/Téléchargements/gejmboj/gejmboj.gb").unwrap();
@@ -109,6 +101,19 @@ where
     let mut mbc = get_mbc::<_, InstantRtc>(Arc::from(rom.into_boxed_slice())).unwrap();
     let mut emulator = Emulator::default();
 
+    let config = StreamConfig {
+        channels: 2,
+        // same as web
+        buffer_size: BufferSize::Fixed(128),
+        ..config
+    };
+
+    let sample_rate = config.sample_rate;
+    let mut sample_index = 0u32;
+
+    let noise = get_noise(false);
+    let short_noise = get_noise(true);
+
     let base = SYSTEM_CLOCK_FREQUENCY / sample_rate;
     let remainder = SYSTEM_CLOCK_FREQUENCY % sample_rate;
     let mut error = 0;
@@ -116,12 +121,12 @@ where
 
     device
         .build_output_stream(
-            config,
+            &config,
             move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
                 if let Ok(input) = shared_joypad.try_read() {
                     *emulator.get_joypad_mut() = *input;
                 }
-                for frame in data.chunks_mut(channels) {
+                for frame in data.as_chunks_mut::<2>().0 {
                     let mut cycles = base;
                     error += remainder;
 
@@ -149,14 +154,10 @@ where
                     let sampler = emulator.get_apu().get_sampler();
 
                     let sample = sample_index as f32 / sample_rate as f32;
-                    let left = T::from_sample(sampler.sample_left(sample, &noise, &short_noise));
-                    let right = T::from_sample(sampler.sample_right(sample, &noise, &short_noise));
+                    frame[0] = T::from_sample(sampler.sample_left(sample, &noise, &short_noise));
+                    frame[1] = T::from_sample(sampler.sample_right(sample, &noise, &short_noise));
                     // 2 minutes without popping (sample_index must not be huge to prevent precision errors)
                     sample_index = sample_index.wrapping_add(1) % (sample_rate * 2 * 60);
-                    for (index, sample) in frame.iter_mut().enumerate() {
-                        // even is left, odd is right
-                        *sample = if index % 2 == 0 { left } else { right }
-                    }
                 }
             },
             |err| eprintln!("an error occurred on stream: {err}"),
