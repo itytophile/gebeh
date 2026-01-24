1546f1e639acb26fc3b23c6ce6e5a88a3f8ea48a
itytophile <imran.tierce@protonmail.com>
2026-01-24 23:30:37 +0100

    simpler length but still passes 2

--- gebeh-core/src/apu/length.rs
+++ gebeh-core/src/apu/length.rs
@@ -5,35 +5,35 @@ pub const MASK_6_BITS: u8 = 0x3f;
 pub struct Length<const MASK: u8> {
     pub is_enable: bool,
     current_timer_value: u8,
-    has_overflowed: bool,
 }
 
 impl<const MASK: u8> Length<MASK> {
     pub fn set_initial_timer_length(&mut self, value: u8) {
         // according to blargg "Length can be reloaded at any time"
-        self.current_timer_value = value & MASK;
-        self.has_overflowed = false;
+        self.current_timer_value = MASK - (value & MASK);
     }
 
     pub fn trigger(&mut self) {
         log::info!("trigger {:?}", self);
-        self.has_overflowed = false;
+        if self.current_timer_value == 0 {
+            self.current_timer_value = MASK;
+        }
     }
 
     // returns true if overflow
     #[must_use]
     pub fn tick(&mut self, cycles: u64, ch: &'static str) -> bool {
         // https://gbdev.io/pandocs/Audio_details.html#div-apu
-        if self.is_enable && !self.has_overflowed {
-            log::info!("{cycles}: {ch} tick! {} / {}", self.current_timer_value, MASK);
-            self.current_timer_value = self.current_timer_value.wrapping_add(1) & MASK;
-            if !self.has_overflowed && self.current_timer_value == 0 {
-                log::info!("{ch} Expired!")
+        if self.is_enable {
+            log::info!("{cycles}: {ch} tick! {}", self.current_timer_value);
+            if let Some(lol) = self.current_timer_value.checked_sub(1) {
+                self.current_timer_value = lol;
+            } else {
+                log::info!("{ch} Expired!");
+                return true;
             }
-            self.has_overflowed = self.current_timer_value == 0;
-            return self.has_overflowed
         }
-        
+
         false
     }
 }
