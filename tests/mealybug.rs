be467671935ad7719089e47aa29f31a213316ad3
itytophile <imran.tierce@protonmail.com>
2026-01-31 17:54:48 +0100

    test read png info

---
+++ tests/mealybug.rs
@@ -0,0 +1,13 @@
+use std::{fs::File, io::BufReader};
+
+#[test]
+fn prout() {
+    let decoder = png::Decoder::new(BufReader::new(File::open("downloads/mealybug-tearoom-tests-master/expected/DMG-blob/m2_win_en_toggle.png").unwrap()));
+    let mut reader = decoder.read_info().unwrap();
+    // Allocate the output buffer.
+    let mut buf = vec![0; reader.output_buffer_size().unwrap()];
+    let info = reader.next_frame(&mut buf).unwrap();
+    // Grab the bytes of the image.
+    let bytes = &buf[..info.buffer_size()];
+    println!("{info:?} {} {}", bytes.len(), buf.len())
+}
