f69a46a9f64c01c975036b977e6e7578e2edc900
itytophile <imran.tierce@protonmail.com>
2026-02-17 20:31:40 +0100

    setup

--- gebeh-server/src/main.rs
+++ gebeh-server/src/main.rs
@@ -1,6 +1,7 @@
 use fastwebsockets::OpCode;
 use fastwebsockets::WebSocketError;
 use fastwebsockets::upgrade;
+use futures_util::TryFutureExt;
 use http_body_util::Empty;
 use hyper::Request;
 use hyper::Response;
@@ -9,6 +10,7 @@ use hyper::body::Incoming;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use tokio::net::TcpListener;
+use tracing_subscriber::fmt::format::FmtSpan;
 
 async fn handle_client(fut: upgrade::UpgradeFut) -> Result<(), WebSocketError> {
     let mut ws = fastwebsockets::FragmentCollector::new(fut.await?);
@@ -40,27 +42,31 @@ async fn server_upgrade(
     Ok(response)
 }
 
-fn main() -> Result<(), WebSocketError> {
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    tracing_subscriber::fmt()
+        .with_span_events(FmtSpan::NEW | FmtSpan::CLOSE)
+        .init();
+
     let rt = tokio::runtime::Builder::new_current_thread()
         .enable_io()
         .build()
         .unwrap();
 
-    rt.block_on(async move {
+    rt.block_on(async {
         let listener = TcpListener::bind("0.0.0.0:8080").await?;
-        println!("Server started, listening on 0.0.0.0:8080");
+        tracing::info!("Listening on 0.0.0.0:8080");
         loop {
             let (stream, _) = listener.accept().await?;
-            println!("Client connected");
-            tokio::spawn(async move {
-                let io = hyper_util::rt::TokioIo::new(stream);
-                let conn_fut = http1::Builder::new()
-                    .serve_connection(io, service_fn(server_upgrade))
-                    .with_upgrades();
-                if let Err(e) = conn_fut.await {
-                    println!("An error occurred: {:?}", e);
-                }
-            });
+            tokio::spawn(
+                http1::Builder::new()
+                    .serve_connection(
+                        hyper_util::rt::TokioIo::new(stream),
+                        service_fn(server_upgrade),
+                    )
+                    .with_upgrades()
+                    .inspect_err(|err| println!("An error occurred: {err:?}")),
+            );
         }
     })
 }
