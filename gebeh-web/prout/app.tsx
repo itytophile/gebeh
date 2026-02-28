420f454fd28ce8adee179333283a7820615421da
itytophile <imran.tierce@protonmail.com>
2026-02-28 23:24:26 +0100

    rom input

--- gebeh-web/prout/app.tsx
+++ gebeh-web/prout/app.tsx
@@ -7,21 +7,8 @@ import startSelect from "../assets/startSelect.svg";
 import Button from "./button";
 import Dpad from "./dpad";
 import Room from "./room";
-import { type FromMainMessage } from "./common.ts";
-import { getSave } from "./saves";
 import initNode from "./init-node.ts";
-
-function getTitleFromRom(rom: Uint8Array): string {
-  const title = rom.slice(0x134, 0x143);
-
-  let endZeroPos = title.indexOf(0);
-  if (endZeroPos === -1) {
-    endZeroPos = title.length;
-  }
-
-  const decoder = new TextDecoder("utf-8", { fatal: true });
-  return decoder.decode(title.slice(0, endZeroPos));
-}
+import RomInput from "./rom-input.tsx";
 
 function App() {
   const [node, setNode] = useState<AudioWorkletNode>();
@@ -36,38 +23,18 @@ function App() {
       </button>
     );
   }
-  return <AppInner port={node.port} />;
+  return <Initialized port={node.port} />;
 }
 
-function AppInner({ port }: { port: MessagePort }) {
+function Initialized({ port }: { port: MessagePort }) {
   return (
     <div className="content">
       <div className="screen">
         <div className="toolbar">
-          <input
-            type="file"
-            onChange={async (event) => {
-              const file = event.target.files?.item(0);
-              if (file) {
-                const bytes = new Uint8Array(await file.arrayBuffer());
-
-                const save = await getSave(getTitleFromRom(new Uint8Array(bytes)));
-                port.postMessage(
-                  {
-                    type: "rom",
-                    bytes,
-                    save,
-                  } satisfies FromMainMessage,
-                  save ? [bytes.buffer, save.buffer] : [bytes.buffer],
-                );
-              } else {
-                console.error("Can't load file");
-              }
-            }}
-          />
+          <RomInput port={port} />
           <Room port={port} />
         </div>
-        {<Canvas port={port} />}
+        <Canvas port={port} />
       </div>
       <div className="buttons-dpads-row">
         <Dpad port={port} />
