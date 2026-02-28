c0c8559b296f150b94285f54a946675f30efe552
itytophile <imran.tierce@protonmail.com>
2026-03-01 00:56:55 +0100

    new check & fix front builds

--- gebeh-web/style.module.css.d.ts
+++ gebeh-web/style.module.css.d.ts
@@ -1,14 +1,13 @@
 declare const styles: {
-  readonly "buttons": string;
-  readonly "buttonsDpadsRow": string;
-  readonly "center": string;
-  readonly "content": string;
-  readonly "flexRow": string;
-  readonly "hidden": string;
-  readonly "interactive": string;
-  readonly "screen": string;
-  readonly "startSelectButtons": string;
-  readonly "toolbar": string;
+  readonly buttons: string;
+  readonly buttonsDpadsRow: string;
+  readonly center: string;
+  readonly content: string;
+  readonly flexRow: string;
+  readonly hidden: string;
+  readonly interactive: string;
+  readonly screen: string;
+  readonly startSelectButtons: string;
+  readonly toolbar: string;
 };
 export = styles;
-
