b1a8d819474a7d2ec58b519920d0b7954ecd6ba0
itytophile <imran.tierce@protonmail.com>
2026-02-28 22:56:46 +0100

    remove index file

--- gebeh-web/prout/app.tsx
+++ gebeh-web/prout/app.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { onLoadFile } from ".";
 import "../style.css";
 import Canvas from "./canvas";
 import buttonA from "../assets/buttonA.svg";
@@ -8,6 +7,106 @@ import startSelect from "../assets/startSelect.svg";
 import Button from "./button";
 import Dpad from "./dpad";
 import { CreatedRoom, JoinedRoom } from "./room";
+import { AUDIO_PROCESSOR_NAME, type FromMainMessage, type FromNodeMessage } from "./common.ts";
+import { getSave, writeSave } from "./saves";
+import workletURL from "./worklet.ts?worker&url";
+import wasm from "../pkg/gebeh_web_bg.wasm?url";
+
+let node: AudioWorkletNode | undefined;
+let isNodeReady = false;
+let notReadyRom: Uint8Array | undefined;
+
+const getAudioWorkletNode = async (): Promise<AudioWorkletNode> => {
+  if (node) {
+    return node;
+  }
+
+  const audioContext = new AudioContext();
+  await audioContext.audioWorklet.addModule(workletURL);
+  node = new AudioWorkletNode(audioContext, AUDIO_PROCESSOR_NAME, {
+    outputChannelCount: [2],
+  });
+  const { port } = node;
+  // addNetwork(port);
+  // addButtons(port);
+  // https://github.com/wasm-bindgen/wasm-bindgen/blob/9ffc52c8d29f006cadf669dcfce6b6f74d308194/examples/synchronous-instantiation/index.html
+  port.addEventListener("message", async ({ data }: MessageEvent<FromNodeMessage>) => {
+    switch (data.type) {
+      case "ready": {
+        // ready
+        isNodeReady = true;
+        if (notReadyRom) {
+          const save = await getSave(getTitleFromRom(new Uint8Array(notReadyRom)));
+          port.postMessage(
+            {
+              type: "rom",
+              bytes: notReadyRom,
+              save,
+            } satisfies FromMainMessage,
+            save ? [notReadyRom.buffer, save.buffer] : [notReadyRom.buffer],
+          );
+        }
+        break;
+      }
+      case "wasm": {
+        console.log("Sending wasm");
+        // https://github.com/wasm-bindgen/wasm-bindgen/blob/9ffc52c8d29f006cadf669dcfce6b6f74d308194/examples/synchronous-instantiation/index.html
+        void fetch(wasm)
+          .then((response) => response.bytes())
+          .then((bytes) => {
+            port.postMessage({ type: "wasm", bytes } satisfies FromMainMessage, [bytes.buffer]);
+          });
+        break;
+      }
+      case "save": {
+        await writeSave(data.title, data.buffer);
+      }
+    }
+  });
+  document.addEventListener("visibilitychange", () => {
+    if (document.visibilityState == "visible") {
+      port.postMessage({ type: "enableMessages" } satisfies FromMainMessage);
+    } else {
+      port.postMessage({ type: "disableMessages" } satisfies FromMainMessage);
+    }
+  });
+  port.start();
+  node.connect(audioContext.destination);
+  return node;
+};
+
+function getTitleFromRom(rom: Uint8Array): string {
+  const title = rom.slice(0x134, 0x143);
+
+  let endZeroPos = title.indexOf(0);
+  if (endZeroPos === -1) {
+    endZeroPos = title.length;
+  }
+
+  const decoder = new TextDecoder("utf-8", { fatal: true });
+  return decoder.decode(title.slice(0, endZeroPos));
+}
+
+const onLoadFile = async (file: File): Promise<AudioWorkletNode> => {
+  const bytes = new Uint8Array(await file.arrayBuffer());
+  const node = await getAudioWorkletNode();
+
+  if (isNodeReady) {
+    const save = await getSave(getTitleFromRom(new Uint8Array(bytes)));
+    node.port.postMessage(
+      {
+        type: "rom",
+        bytes,
+        save,
+      } satisfies FromMainMessage,
+      save ? [bytes.buffer, save.buffer] : [bytes.buffer],
+    );
+  } else {
+    notReadyRom = bytes;
+  }
+
+  return node;
+};
 
 function App() {
   const [node, setNode] = useState<AudioWorkletNode>();
