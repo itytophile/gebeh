1318965c2540130bac0517a7dc731fb10237e199
itytophile <imran.tierce@protonmail.com>
2026-03-01 23:38:21 +0100

    disabled bulma button

--- gebeh-web/app/bulma/button.tsx
+++ gebeh-web/app/bulma/button.tsx
@@ -6,14 +6,16 @@ function Button({
   label,
   onClick,
   color,
+  disabled,
 }: {
   icon?: IconLookup;
   label: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   color?: "is-success" | "is-danger" | "is-link" | "is-info";
+  disabled?: boolean;
 }) {
   return (
-    <button className={"button" + (color ? " " + color : "")} onClick={onClick}>
+    <button className={"button" + (color ? " " + color : "")} onClick={onClick} disabled={disabled}>
       {icon && (
         <span className="icon">
           <FontAwesomeIcon icon={icon} />
