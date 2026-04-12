27b0cab616671640032391391648c14ef6707917
itytophile <imran.tierce@protonmail.com>
2026-04-12 11:59:22 +0200

    split files

---
+++ gebeh-web/app/multiplayer/room.tsx
@@ -0,0 +1,244 @@
+import { useState, useEffect } from "react";
+import type { FromNodeMessage, FromMainMessage } from "../common";
+import Button from "../bulma/button";
+import { faRocket, faPlug, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import { WebRtcMultiplayer, WebRtcMultiplayerOfferer } from "./webrtc";
+import { getBinaryMessage, getTextMessage, websocketGenerator, type WsAndMessages } from "./ws";
+
+function Room({ port }: { port: MessagePort }) {
+  const [room, setRoom] = useState<
+    { type: "input"; value: string } | { type: "created" } | { type: "joined"; name: string }
+  >({ type: "input", value: "" });
+  const [isWebRtcEnabled, setIsWebRtcEnabled] = useState(false);
+
+  if (room.type === "input") {
+    return (
+      <>
+        <div className="field">
+          <label className="checkbox">
+            <input
+              type="checkbox"
+              checked={isWebRtcEnabled}
+              onChange={() => {
+                setIsWebRtcEnabled((a) => !a);
+              }}
+            />
+            {" Enable WebRTC"}
+          </label>
+        </div>
+        <div className="field">
+          <Button
+            onClick={() => {
+              setRoom({ type: "created" });
+            }}
+            label="Create room"
+            color="is-success"
+            icon={faRocket}
+          />
+        </div>
+        <div className="field has-addons">
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              placeholder="Room to join"
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  setRoom({ type: "joined", name: room.value });
+                }
+              }}
+              onChange={(event) => {
+                setRoom({ type: "input", value: event.target.value });
+              }}
+            />
+          </div>
+          <div className="control">
+            <Button
+              label="Join room"
+              color="is-info"
+              onClick={() => {
+                setRoom({ type: "joined", name: room.value });
+              }}
+              icon={faPlug}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  const button =
+    room.type === "created" ? (
+      <CreatedRoom port={port} isWebRtcEnabled={isWebRtcEnabled} />
+    ) : (
+      <JoinedRoom port={port} room={room.name} isWebRtcEnabled={isWebRtcEnabled} />
+    );
+
+  return (
+    <div className="field has-addons">
+      <div className="control">{button}</div>
+      <div className="control">
+        <Button
+          label="Reset"
+          color="is-warning"
+          icon={faRotateLeft}
+          onClick={() => {
+            setRoom({ type: "input", value: "" });
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function CreatedRoom({ port, isWebRtcEnabled }: { port: MessagePort; isWebRtcEnabled: boolean }) {
+  const [status, setStatus] = useState<
+    | { type: "loading" }
+    | { type: "closed" }
+    | {
+        type: "ready";
+        room: string;
+        ws: WsAndMessages;
+      }
+    | { type: "waiting"; room: string }
+  >({ type: "loading" });
+
+  useEffect(() => {
+    const ws = new WebSocket(`${globalThis.location.protocol}//${globalThis.location.host}/ws`);
+    ws.binaryType = "arraybuffer";
+    const messages = websocketGenerator(ws);
+
+    ws.addEventListener("close", () => {
+      setStatus({ type: "closed" });
+    });
+
+    void (async () => {
+      const room = await getTextMessage(messages);
+      if (!room) {
+        return;
+      }
+      setStatus({ type: "waiting", room });
+      if (!(await getBinaryMessage(messages))) {
+        return;
+      }
+      // empty message, it means that guest is here
+      setStatus({ type: "ready", room, ws: { inner: ws, messages } });
+    })();
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  if (status.type === "loading") {
+    return <Button label="Loading..." />;
+  }
+
+  if (status.type === "closed") {
+    return <Button label="Room closed 🍗🍗" />;
+  }
+
+  if (status.type === "waiting") {
+    return (
+      <Button
+        label={`${status.room} 🥚🐔`}
+        onClick={() => {
+          void navigator.clipboard.writeText(status.room);
+        }}
+      />
+    );
+  }
+
+  return isWebRtcEnabled ? (
+    <WebRtcMultiplayerOfferer port={port} ws={status.ws} />
+  ) : (
+    <WebSocketMultiplayer port={port} ws={status.ws} />
+  );
+}
+
+function JoinedRoom({
+  room,
+  port,
+  isWebRtcEnabled,
+}: {
+  room: string;
+  port: MessagePort;
+  isWebRtcEnabled: boolean;
+}) {
+  const [status, setStatus] = useState<
+    { type: "loading" } | { type: "ready"; ws: WsAndMessages } | { type: "closed" }
+  >({ type: "loading" });
+  useEffect(() => {
+    const ws = new WebSocket(
+      `${globalThis.location.protocol}//${globalThis.location.host}/ws?room=${room}`,
+    );
+    ws.binaryType = "arraybuffer";
+    const messages = websocketGenerator(ws);
+    ws.addEventListener("open", () => {
+      setStatus({ type: "ready", ws: { inner: ws, messages } });
+    });
+    ws.addEventListener("close", () => {
+      setStatus({ type: "closed" });
+    });
+
+    return () => {
+      ws.close();
+    };
+  }, [room]);
+
+  if (status.type === "loading") {
+    return <Button label="Loading..." />;
+  }
+
+  if (status.type === "closed") {
+    return <Button label="Room closed 🍗🍗" />;
+  }
+
+  return isWebRtcEnabled ? (
+    <WebRtcMultiplayer port={port} ws={status.ws} />
+  ) : (
+    <WebSocketMultiplayer port={port} ws={status.ws} />
+  );
+}
+
+export default Room;
+
+function WebSocketMultiplayer({ port, ws }: { port: MessagePort; ws: WsAndMessages }) {
+  useEffect(() => {
+    const portListener = ({ data }: MessageEvent<FromNodeMessage>) => {
+      if (data.type === "serial") {
+        ws.inner.send(data.buffer);
+      }
+    };
+
+    void (async () => {
+      for await (const message of ws.messages) {
+        if (!(message.data instanceof ArrayBuffer)) {
+          throw new TypeError("Only binary messages are accepted");
+        }
+        port.postMessage(
+          {
+            type: "serial",
+            buffer: new Uint8Array(message.data),
+          } satisfies FromMainMessage,
+          [message.data],
+        );
+      }
+    })();
+
+    port.addEventListener("message", portListener);
+
+    port.postMessage({
+      type: "serialConnected",
+    } satisfies FromMainMessage);
+
+    return () => {
+      port.postMessage({
+        type: "serialDisconnected",
+      } satisfies FromMainMessage);
+      port.removeEventListener("message", portListener);
+    };
+  }, [ws, port]);
+
+  return <Button label="Connected 🐣🐔" />;
+}
---
+++ gebeh-web/app/multiplayer/webrtc.tsx
@@ -0,0 +1,160 @@
+import { useEffect, useState } from "react";
+import type { FromMainMessage, FromNodeMessage } from "../common";
+import { getTextMessage, type WsAndMessages } from "./ws";
+
+export function WebRtcMultiplayer({ port, ws }: { port: MessagePort; ws: WsAndMessages }) {
+  const [channel, setChannel] = useState<RTCDataChannel>();
+
+  useEffect(() => {
+    const pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.l.google.com:19302",
+        },
+      ],
+    });
+
+    const icecandidateListener = (event: RTCPeerConnectionIceEvent) => {
+      if (event.candidate) {
+        const text = JSON.stringify({ candidate: event.candidate });
+        ws.inner.send(text);
+      }
+    };
+
+    pc.addEventListener("icecandidate", icecandidateListener);
+
+    void (async () => {
+      while (true) {
+        const text = await getTextMessage(ws.messages);
+        if (!text) {
+          return;
+        }
+        const parsed = JSON.parse(text) as
+          | { offer: RTCSessionDescriptionInit }
+          | { candidate: RTCIceCandidate };
+        if ("candidate" in parsed) {
+          console.log("candidate");
+          await pc.addIceCandidate(parsed.candidate);
+          continue;
+        }
+        pc.ondatachannel = (event) => {
+          console.log("new channel", event.channel.label);
+          const receiveChannel = event.channel;
+          receiveChannel.binaryType = "arraybuffer";
+          setChannel(receiveChannel);
+        };
+        console.log("offer received");
+        await pc.setRemoteDescription(new RTCSessionDescription(parsed.offer));
+        const answer = await pc.createAnswer();
+        await pc.setLocalDescription(answer);
+        ws.inner.send(JSON.stringify({ answer }));
+      }
+    })();
+
+    return () => {
+      pc.close();
+    };
+  }, [ws]);
+
+  return channel ? <DataChannelHandler channel={channel} port={port} /> : "rtc wait";
+}
+
+export function WebRtcMultiplayerOfferer({ port, ws }: { port: MessagePort; ws: WsAndMessages }) {
+  const [channel, setChannel] = useState<RTCDataChannel>();
+
+  useEffect(() => {
+    const pc = new RTCPeerConnection({
+      iceServers: [
+        {
+          urls: "stun:stun.l.google.com:19302",
+        },
+      ],
+    });
+
+    const icecandidateListener = (event: RTCPeerConnectionIceEvent) => {
+      if (event.candidate) {
+        const text = JSON.stringify({ candidate: event.candidate });
+        ws.inner.send(text);
+      }
+    };
+
+    pc.addEventListener("icecandidate", icecandidateListener);
+
+    const dataChannel = pc.createDataChannel("prout");
+    dataChannel.binaryType = "arraybuffer";
+
+    void (async () => {
+      while (true) {
+        const text = await getTextMessage(ws.messages);
+        if (!text) {
+          return;
+        }
+        const parsed = JSON.parse(text) as
+          | { answer: RTCSessionDescriptionInit }
+          | { candidate: RTCIceCandidate };
+        if ("candidate" in parsed) {
+          console.log("candidate");
+          await pc.addIceCandidate(parsed.candidate);
+          continue;
+        }
+        console.log("answer received");
+        await pc.setRemoteDescription(new RTCSessionDescription(parsed.answer));
+        setChannel(dataChannel);
+      }
+    })();
+
+    void pc.createOffer().then(async (offer) => {
+      console.log("offer created");
+      await pc.setLocalDescription(offer);
+      ws.inner.send(JSON.stringify({ offer }));
+    });
+
+    return () => {
+      pc.close();
+    };
+  }, [ws]);
+
+  return channel ? <DataChannelHandler channel={channel} port={port} /> : "rtc wait";
+}
+
+function DataChannelHandler({ channel, port }: { channel: RTCDataChannel; port: MessagePort }) {
+  useEffect(() => {
+    port.postMessage({
+      type: "serialConnected",
+    } satisfies FromMainMessage);
+
+    const portListener = ({ data }: MessageEvent<FromNodeMessage>) => {
+      if (data.type === "serial") {
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-arguments
+        channel.send(data.buffer as Uint8Array<ArrayBuffer>);
+      }
+    };
+
+    const onWebRtcMessage = (message: MessageEvent<unknown>) => {
+      if (!(message.data instanceof ArrayBuffer)) {
+        throw new TypeError("Only binary messages are accepted");
+      }
+      port.postMessage(
+        {
+          type: "serial",
+          buffer: new Uint8Array(message.data),
+        } satisfies FromMainMessage,
+        [message.data],
+      );
+    };
+
+    port.addEventListener("message", portListener);
+
+    channel.addEventListener("message", onWebRtcMessage);
+
+    return () => {
+      channel.close();
+      port.removeEventListener("message", portListener);
+      port.postMessage({
+        type: "serialDisconnected",
+      } satisfies FromMainMessage);
+    };
+  }, [channel, port]);
+
+  return "rtc connected";
+}
