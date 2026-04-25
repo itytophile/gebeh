653194a8cfab4899efd22aeaf28bb55846a5f37c
itytophile <imran.tierce@protonmail.com>
2026-04-25 18:49:03 +0200

    Dffr

--- gebeh-system-verilog/src/main.rs
+++ gebeh-system-verilog/src/main.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 use sv_parser::{
     Description, Expression, Identifier, InstanceIdentifier, ListOfPortConnections, Locate,
     ModuleDeclaration, ModuleOrGenerateItem, NamedPortConnection, NonPortModuleItem, Primary,
-    RefNode, SourceText, SyntaxTree, parse_sv, unwrap_node,
+    RefNode, SyntaxTree, parse_sv,
 };
 
 fn main() {
@@ -22,6 +22,12 @@ fn main() {
 
     let (syntax_tree, _) = result.unwrap();
 
+    for dffr in get_instances(&syntax_tree) {
+        println!("{dffr:?}");
+    }
+}
+
+fn get_instances<'a>(syntax_tree: &'a SyntaxTree) -> impl Iterator<Item = Dffr<'a>> {
     let a = syntax_tree.into_iter().next().unwrap();
     let RefNode::SourceText(a) = a else { panic!() };
     let Description::ModuleDeclaration(a) = &a.nodes.2[0] else {
@@ -30,93 +36,87 @@ fn main() {
     let ModuleDeclaration::Ansi(a) = a.as_ref() else {
         panic!()
     };
-    let mut i = 0;
-    for a in &a.nodes.2 {
-        let NonPortModuleItem::ModuleOrGenerateItem(a) = a else {
-            panic!()
-        };
-        let ModuleOrGenerateItem::Module(a) = a.as_ref() else {
-            continue;
-        };
-        let (module_id, _, instances, _) = &a.nodes.1.nodes;
-        let name = get_name_from_identifier(&syntax_tree, &module_id.nodes.0);
-        if !name.contains("dffr") {
-            continue;
-        }
-        println!("{name}");
-        let instance = &instances.nodes.0;
-        let (name, connections) = &instance.nodes;
-        let (InstanceIdentifier { nodes: (id,) }, _) = &name.nodes;
-        let locate = get_locate_from_identifier(id);
-        let id = syntax_tree.get_str(locate).unwrap();
-        println!("{} {id}", locate.line);
-        let ListOfPortConnections::Named(named) = connections.nodes.1.as_ref().unwrap() else {
-            continue;
-        };
-        for a in named.nodes.0.contents() {
-            let NamedPortConnection::Identifier(a) = a else {
+    a.nodes
+        .2
+        .iter()
+        .filter_map(move |a| {
+            let NonPortModuleItem::ModuleOrGenerateItem(a) = a else {
                 panic!()
             };
-            let (_, _, id, expression) = &a.nodes;
-            let expression = &expression.as_ref().unwrap().nodes.1.as_ref().map(|expr| {
-                let Expression::Primary(expr) = expr else {
-                    panic!()
-                };
-                let Primary::Hierarchical(expr) = expr.as_ref() else {
-                    panic!()
-                };
-                let (_, id, _) = &expr.nodes;
-                let (_, _, id) = &id.nodes;
-                get_name_from_identifier(&syntax_tree, id)
-            });
-            let locate = get_locate_from_identifier(&id.nodes.0);
+            let ModuleOrGenerateItem::Module(a) = a.as_ref() else {
+                return None;
+            };
+            let (module_id, _, instances, _) = &a.nodes.1.nodes;
+            let name = get_name_from_identifier(syntax_tree, &module_id.nodes.0);
+            if name != "dmg_dffr" {
+                return None;
+            }
+            println!("{name}");
+            let instance = &instances.nodes.0;
+            let (name, connections) = &instance.nodes;
+            let (InstanceIdentifier { nodes: (id,) }, _) = &name.nodes;
+            let locate = get_locate_from_identifier(id);
             let id = syntax_tree.get_str(locate).unwrap();
-            println!("=> {} {id} {expression:?}", locate.line);
-        }
+            let ListOfPortConnections::Named(named) = connections.nodes.1.as_ref().unwrap() else {
+                panic!();
+            };
+            Some((id, named))
+        })
+        .map(move |(id, named)| {
+            let mut d = None;
+            let mut clk = None;
+            let mut r_n = None;
+            let mut q = None;
+            let mut q_n = None;
 
-        i += 1;
-    }
-    println!("count {i}");
+            for a in named.nodes.0.contents() {
+                let NamedPortConnection::Identifier(a) = a else {
+                    panic!()
+                };
+                let (_, _, id, expression) = &a.nodes;
+                let expression = &expression.as_ref().unwrap().nodes.1.as_ref().map(|expr| {
+                    let Expression::Primary(expr) = expr else {
+                        panic!()
+                    };
+                    let Primary::Hierarchical(expr) = expr.as_ref() else {
+                        panic!()
+                    };
+                    let (_, id, _) = &expr.nodes;
+                    let (_, _, id) = &id.nodes;
+                    get_name_from_identifier(syntax_tree, id)
+                });
+                let locate = get_locate_from_identifier(&id.nodes.0);
+                let id = syntax_tree.get_str(locate).unwrap();
 
-    // &SyntaxTree is iterable
-    //     for node in &syntax_tree {
-    //         if let Some(locate) = get_identifier(node.clone()) {
-    // println!("{} {node} {}", locate.line, syntax_tree.get_str(&locate).unwrap());
-    //         }
+                match id {
+                    "d" => d = *expression,
+                    "clk" => clk = *expression,
+                    "r_n" => r_n = *expression,
+                    "q" => q = *expression,
+                    "q_n" => q_n = *expression,
+                    _ => panic!(),
+                }
+            }
 
-    //         let RefNode::HierarchicalInstance(instance) = node else {
-    //             continue;
-    //         };
+            Dffr {
+                d: d.unwrap(),
+                name: id,
+                clk: clk.unwrap(),
+                q,
+                q_n,
+                r_n: r_n.unwrap(),
+            }
+        })
+}
 
-    //         let (name, connections) = &instance.nodes;
-    //         let (InstanceIdentifier { nodes: (id,) }, _) = &name.nodes;
-    //         let locate = get_locate_from_identifier(id);
-    //         let id = syntax_tree.get_str(locate).unwrap();
-    //         println!("{} {id}", locate.line);
-    //         let ListOfPortConnections::Named(named) = connections.nodes.1.as_ref().unwrap() else {
-    //             continue;
-    //         };
-    //         for a in named.nodes.0.contents() {
-    //             let NamedPortConnection::Identifier(a) = a else {
-    //                 panic!()
-    //             };
-    //             let (_, _, id, expression) = &a.nodes;
-    //             let expression = &expression.as_ref().unwrap().nodes.1.as_ref().map(|expr| {
-    //                 let Expression::Primary(expr) = expr else {
-    //                     panic!()
-    //                 };
-    //                 let Primary::Hierarchical(expr) = expr.as_ref() else {
-    //                     panic!()
-    //                 };
-    //                 let (_, id, _) = &expr.nodes;
-    //                 let (_, _, id) = &id.nodes;
-    //                 get_name_from_identifier(&syntax_tree, id)
-    //             });
-    //             let locate = get_locate_from_identifier(&id.nodes.0);
-    //             let id = syntax_tree.get_str(locate).unwrap();
-    //             println!("=> {} {id} {expression:?}", locate.line);
-    //         }
-    //     }
+#[derive(Debug)]
+struct Dffr<'a> {
+    name: &'a str,
+    d: &'a str,
+    clk: &'a str,
+    r_n: &'a str,
+    q: Option<&'a str>,
+    q_n: Option<&'a str>,
 }
 
 fn get_locate_from_identifier(id: &Identifier) -> &Locate {
@@ -129,13 +129,4 @@ fn get_locate_from_identifier(id: &Identifier) -> &Locate {
 
 fn get_name_from_identifier<'a>(syntax_tree: &'a SyntaxTree, id: &Identifier) -> &'a str {
     syntax_tree.get_str(get_locate_from_identifier(id)).unwrap()
-}
-
-fn get_identifier(node: RefNode) -> Option<Locate> {
-    // unwrap_node! can take multiple types
-    match unwrap_node!(node, SimpleIdentifier, EscapedIdentifier) {
-        Some(RefNode::SimpleIdentifier(x)) => Some(x.nodes.0),
-        Some(RefNode::EscapedIdentifier(x)) => Some(x.nodes.0),
-        _ => None,
-    }
 }
