62b69dc52f3187882f40c039bbf77da9403e7ec2
itytophile <imran.tierce@protonmail.com>
2026-04-26 11:27:59 +0200

    parse_nor_latch

--- gebeh-system-verilog/src/nor_latch.rs
+++ gebeh-system-verilog/src/nor_latch.rs
@@ -1,7 +1,42 @@
+use sv_parser::{HierarchicalInstance, SyntaxTree};
+
+use crate::{extract_id_and_ports, get_ports};
+
 #[derive(Debug)]
-struct NorLatch<'a> {
+pub struct NorLatch<'a> {
+    name: &'a str,
     s: &'a str,
     r: &'a str,
     q: Option<&'a str>,
     q_n: Option<&'a str>,
+}
+
+pub fn parse_nor_latch<'a>(
+    syntax_tree: &'a SyntaxTree,
+    instance: &'a HierarchicalInstance,
+) -> NorLatch<'a> {
+    let (id, named) = extract_id_and_ports(syntax_tree, instance);
+
+    let mut s = None;
+    let mut r = None;
+    let mut q = None;
+    let mut q_n = None;
+
+    for (id, expression) in get_ports(syntax_tree, named) {
+        match id {
+            "s" => s = expression,
+            "r" => r = expression,
+            "q" => q = expression,
+            "q_n" => q_n = expression,
+            _ => panic!(),
+        }
+    }
+
+    NorLatch {
+        name: id,
+        s: s.unwrap(),
+        r: r.unwrap(),
+        q,
+        q_n,
+    }
 }
