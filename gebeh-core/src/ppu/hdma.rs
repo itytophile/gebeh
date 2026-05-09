8e6a2eb3767b25282578f0e8a45ed7668f2b6f1a
itytophile <imran.tierce@protonmail.com>
2026-05-09 19:07:30 +0200

    HdmaRegs trait

--- gebeh-core/src/ppu/hdma.rs
+++ gebeh-core/src/ppu/hdma.rs
@@ -29,63 +29,6 @@ pub struct Hdma {
 }
 
 impl Hdma {
-    pub fn write_source_address_low(&mut self, value: u8) {
-        let [high, _] = self.source_address.to_be_bytes();
-        self.source_address = u16::from_be_bytes([high, value]);
-    }
-
-    pub fn write_source_address_high(&mut self, value: u8) {
-        let [_, low] = self.source_address.to_be_bytes();
-        // Citation: The four lower bits of this address will be ignored and treated as 0.
-        self.source_address = u16::from_be_bytes([value, low & 0xf0]);
-    }
-
-    pub fn write_destination_address_low(&mut self, value: u8) {
-        let [high, _] = self.destination_address.to_be_bytes();
-        // Citation: the upper 3 bits are ignored either
-        self.destination_address = u16::from_be_bytes([high & 0x1f, value]);
-    }
-
-    pub fn write_destination_address_high(&mut self, value: u8) {
-        let [_, low] = self.destination_address.to_be_bytes();
-        // Citation: The four lower bits of this address will be ignored and treated as 0.
-        self.destination_address = u16::from_be_bytes([value, low & 0xf0]);
-    }
-
-    pub fn write_length_mode_start(&mut self, value: u8) {
-        // Citation: the lower 7 bits of which specify the Transfer Length (divided by $10, minus 1)
-        self.length = (u16::from(value & 0x7f) + 1) << 4;
-        match (&self.state, value >> 7) {
-            (HdmaState::Inactive, 0) => {
-                self.state = HdmaState::GeneralPurpose(CopyCursor {
-                    src: self.source_address,
-                    dst: self.destination_address,
-                })
-            }
-            (HdmaState::Inactive, 1) => {
-                self.state = HdmaState::HBlank(CopyCursor {
-                    src: self.source_address,
-                    dst: self.destination_address,
-                })
-            }
-            (HdmaState::GeneralPurpose(_), _) => panic!("The CPU is supposed to be halted"),
-            // Citation: It is also possible to terminate an active HBlank transfer by writing zero to Bit 7 of FF55
-            (HdmaState::HBlank(_), 0) => self.state = HdmaState::Inactive,
-            _ => {}
-        };
-    }
-
-    pub fn read_mode_and_length(&self) -> u8 {
-        // Citation: Reading Bit 7 of FF55 can be used to confirm if the DMA transfer is active (1=Not Active, 0=Active).
-        let last_bit = match self.state {
-            HdmaState::Inactive => 1,
-            HdmaState::GeneralPurpose(_) => panic!("The CPU is supposed to be halted"),
-            HdmaState::HBlank(_) => 0,
-        };
-
-        (u8::try_from((self.length >> 4).wrapping_sub(1)).unwrap() & 0x7f) | (last_bit << 7)
-    }
-
     // Citation: In both Normal Speed and Double Speed Mode it takes about 8 μs to transfer a block of $10 bytes.
     // That is, 8 M-cycles in Normal Speed Mode, and 16 “fast” M-cycles in Double Speed Mode.
     /// Returns true if HDMA has performed a transfer, false otherwise.
@@ -120,5 +63,89 @@ impl Hdma {
         }
 
         true
+    }
+}
+
+pub trait HdmaRegs {
+    fn write_source_address_low(&mut self, value: u8);
+    fn write_source_address_high(&mut self, value: u8);
+    fn write_destination_address_low(&mut self, value: u8);
+    fn write_destination_address_high(&mut self, value: u8);
+    fn write_length_mode_start(&mut self, value: u8);
+    fn read_mode_and_length(&self) -> u8;
+}
+
+impl HdmaRegs for Hdma {
+    fn write_source_address_low(&mut self, value: u8) {
+        let [high, _] = self.source_address.to_be_bytes();
+        self.source_address = u16::from_be_bytes([high, value]);
+    }
+
+    fn write_source_address_high(&mut self, value: u8) {
+        let [_, low] = self.source_address.to_be_bytes();
+        // Citation: The four lower bits of this address will be ignored and treated as 0.
+        self.source_address = u16::from_be_bytes([value, low & 0xf0]);
+    }
+
+    fn write_destination_address_low(&mut self, value: u8) {
+        let [high, _] = self.destination_address.to_be_bytes();
+        // Citation: the upper 3 bits are ignored either
+        self.destination_address = u16::from_be_bytes([high & 0x1f, value]);
+    }
+
+    fn write_destination_address_high(&mut self, value: u8) {
+        let [_, low] = self.destination_address.to_be_bytes();
+        // Citation: The four lower bits of this address will be ignored and treated as 0.
+        self.destination_address = u16::from_be_bytes([value, low & 0xf0]);
+    }
+
+    fn write_length_mode_start(&mut self, value: u8) {
+        // Citation: the lower 7 bits of which specify the Transfer Length (divided by $10, minus 1)
+        self.length = (u16::from(value & 0x7f) + 1) << 4;
+        match (&self.state, value >> 7) {
+            (HdmaState::Inactive, 0) => {
+                self.state = HdmaState::GeneralPurpose(CopyCursor {
+                    src: self.source_address,
+                    dst: self.destination_address,
+                })
+            }
+            (HdmaState::Inactive, 1) => {
+                self.state = HdmaState::HBlank(CopyCursor {
+                    src: self.source_address,
+                    dst: self.destination_address,
+                })
+            }
+            (HdmaState::GeneralPurpose(_), _) => panic!("The CPU is supposed to be halted"),
+            // Citation: It is also possible to terminate an active HBlank transfer by writing zero to Bit 7 of FF55
+            (HdmaState::HBlank(_), 0) => self.state = HdmaState::Inactive,
+            _ => {}
+        };
+    }
+
+    fn read_mode_and_length(&self) -> u8 {
+        // Citation: Reading Bit 7 of FF55 can be used to confirm if the DMA transfer is active (1=Not Active, 0=Active).
+        let last_bit = match self.state {
+            HdmaState::Inactive => 1,
+            HdmaState::GeneralPurpose(_) => panic!("The CPU is supposed to be halted"),
+            HdmaState::HBlank(_) => 0,
+        };
+
+        (u8::try_from((self.length >> 4).wrapping_sub(1)).unwrap() & 0x7f) | (last_bit << 7)
+    }
+}
+
+impl HdmaRegs for () {
+    fn write_source_address_low(&mut self, _: u8) {}
+
+    fn write_source_address_high(&mut self, _: u8) {}
+
+    fn write_destination_address_low(&mut self, _: u8) {}
+
+    fn write_destination_address_high(&mut self, _: u8) {}
+
+    fn write_length_mode_start(&mut self, _: u8) {}
+
+    fn read_mode_and_length(&self) -> u8 {
+        0xff
     }
 }
