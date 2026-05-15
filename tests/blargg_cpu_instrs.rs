82cff54c104f1a0bd5dfd095d0698acd399f3216
itytophile <imran.tierce@protonmail.com>
2026-05-15 13:07:45 +0200

    blargg cpu instrs cgb

--- tests/blargg_cpu_instrs.rs
+++ tests/blargg_cpu_instrs.rs
@@ -1,11 +1,11 @@
 use crate::common::machine_to_serial_iter;
 use gebeh::InstantRtc;
-use gebeh_core::{Dmg, Emulator};
+use gebeh_core::{Cgb, Dmg, Emulator, Model};
 use gebeh_front_helper::get_mbc;
 
 mod common;
 
-fn cpu_instrs(name: &str) {
+fn cpu_instrs<M: Model>(name: &str) {
     let expected = format!("{name}\n\n\nPassed\n");
     let len = expected.len();
 
@@ -15,7 +15,7 @@ fn cpu_instrs(name: &str) {
     .unwrap();
     let rom = rom.as_slice();
     let (_, mut mbc) = get_mbc(rom, InstantRtc::default()).unwrap();
-    let mut machine = Emulator::<Dmg>::default();
+    let mut machine = Emulator::<M>::default();
 
     let buffer: Vec<_> = machine_to_serial_iter(&mut machine, mbc.as_mut())
         .take(len)
@@ -26,55 +26,112 @@ fn cpu_instrs(name: &str) {
 
 #[test]
 fn special() {
-    cpu_instrs("01-special");
+    cpu_instrs::<Dmg>("01-special");
 }
 
 #[test]
 fn interrupts() {
-    cpu_instrs("02-interrupts");
+    cpu_instrs::<Dmg>("02-interrupts");
 }
 
 #[test]
 fn op_sp_hl() {
-    cpu_instrs("03-op sp,hl");
+    cpu_instrs::<Dmg>("03-op sp,hl");
 }
 
 #[test]
 fn op_r_imm() {
-    cpu_instrs("04-op r,imm");
+    cpu_instrs::<Dmg>("04-op r,imm");
 }
 
 #[test]
 fn op_rp() {
-    cpu_instrs("05-op rp");
+    cpu_instrs::<Dmg>("05-op rp");
 }
 
 #[test]
 fn ld_r_r() {
-    cpu_instrs("06-ld r,r");
+    cpu_instrs::<Dmg>("06-ld r,r");
 }
 
 #[test]
 fn jr_jp_call_ret_rst() {
-    cpu_instrs("07-jr,jp,call,ret,rst");
+    cpu_instrs::<Dmg>("07-jr,jp,call,ret,rst");
 }
 
 #[test]
 fn misc_instrs() {
-    cpu_instrs("08-misc instrs");
+    cpu_instrs::<Dmg>("08-misc instrs");
 }
 
 #[test]
 fn op_r_r() {
-    cpu_instrs("09-op r,r");
+    cpu_instrs::<Dmg>("09-op r,r");
 }
 
 #[test]
 fn bit_ops() {
-    cpu_instrs("10-bit ops");
+    cpu_instrs::<Dmg>("10-bit ops");
 }
 
 #[test]
 fn op_a_hl() {
-    cpu_instrs("11-op a,(hl)");
+    cpu_instrs::<Dmg>("11-op a,(hl)");
+}
+
+// cgb
+
+#[test]
+fn special_cgb() {
+    cpu_instrs::<Cgb>("01-special");
+}
+
+#[test]
+fn interrupts_cgb() {
+    cpu_instrs::<Cgb>("02-interrupts");
+}
+
+#[test]
+fn op_sp_hl_cgb() {
+    cpu_instrs::<Cgb>("03-op sp,hl");
+}
+
+#[test]
+fn op_r_imm_cgb() {
+    cpu_instrs::<Cgb>("04-op r,imm");
+}
+
+#[test]
+fn op_rp_cgb() {
+    cpu_instrs::<Cgb>("05-op rp");
+}
+
+#[test]
+fn ld_r_r_cgb() {
+    cpu_instrs::<Cgb>("06-ld r,r");
+}
+
+#[test]
+fn jr_jp_call_ret_rst_cgb() {
+    cpu_instrs::<Cgb>("07-jr,jp,call,ret,rst");
+}
+
+#[test]
+fn misc_instrs_cgb() {
+    cpu_instrs::<Cgb>("08-misc instrs");
+}
+
+#[test]
+fn op_r_r_cgb() {
+    cpu_instrs::<Cgb>("09-op r,r");
+}
+
+#[test]
+fn bit_ops_cgb() {
+    cpu_instrs::<Cgb>("10-bit ops");
+}
+
+#[test]
+fn op_a_hl_cgb() {
+    cpu_instrs::<Cgb>("11-op a,(hl)");
 }
